632c268f44e5308d3755db3d90080bea0a1fc1fe
MananPatel6902 <manan06092002@gmail.com>
2024-05-20 15:16:31 +0530

    added try-catch and Promise.all to handle the errors

--- packages/core/src/orchestrator.js
+++ packages/core/src/orchestrator.js
@@ -34,34 +34,26 @@ async function orchestrator({
   },
   metaData
 }) {
-  // loadRuleConfig from server
-  // receive the config from serer
-  // findStartFile
-  // create a dependency graph
-  // load files
-  // for each file
-  // create ast
-  // call runRules with ast and config
-  // for each config find the corresponding rule
-  // and generate the data
-  // store the data in memory
-  // run it for all files(jsx, js, ts, tsx)
-  // collect data for all files
-  // send the data
-  // exit the process
 
-  const ruleConfigs = await loadRuleConfigs(ruleRequestApiUrl, ruleRequestConfigPath);
-  const startFile = findStartFile(entryFile, baseDir);
-  const files = await getDependencyTree(startFile, baseDir);
+ 
+  try {
+    const ruleConfigs = await loadRuleConfigs(ruleRequestApiUrl, ruleRequestConfigPath);
+    const startFile = findStartFile(entryFile, baseDir);
+    const files = await getDependencyTree(startFile, baseDir);
 
-  for (const file of files) {
-    const {ast, language} = parser(file, baseDir);
-    const finalResponse = runRules({ast,ruleConfigs, language, file, metaData, ruleDefinitionPath});
-    accumulateResponse(finalResponse);
+    await Promise.all(files.map(async (file) => {
+      const { ast, language } = parser(file, baseDir);
+      const finalResponse = runRules({ ast, ruleConfigs, language, file, metaData, ruleDefinitionPath });
+      accumulateResponse(finalResponse);
+    }));
+
+    const response = getFinalResponse();
+    await postData(ruleResponseApiUrl, response);
+  } catch (error) {
+    console.error('Error in orchestrator:', error);
+    throw error;
   }
 
-  const response = getFinalResponse();
-  postData(ruleResponseApiUrl, response);
 }
 
 module.exports = orchestrator;
